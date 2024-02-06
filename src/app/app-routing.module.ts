b21ee1f257d62063fdc6e88855daf412272590c4
Wlahmamssi69 <wafa.lahmamssi@outlook.com>
2024-02-06 14:18:56 +0100

    setup the project and added bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'',
+    redirectTo:'products',
+    pathMatch:'full'
+  },
+  {
+    path:'products',
+    component:ProductListComponent
+  },
+  {
+    path:'add-product',
+    component:AddProductComponent
+  },{
+    path:'productList',
+    component:ProductListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
